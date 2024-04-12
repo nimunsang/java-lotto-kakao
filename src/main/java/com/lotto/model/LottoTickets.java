d892d884f115fe61c459bcbb139f281dd87df7b7
zero.k <sk980919@naver.com>
2024-04-12 10:47:43 +0900

    refactor: 사용하지 않는 코드 제거

--- src/main/java/com/lotto/model/LottoTickets.java
+++ src/main/java/com/lotto/model/LottoTickets.java
@@ -8,13 +8,6 @@ public class LottoTickets {
 
     private final List<LottoTicket> lottoTickets = new ArrayList<>();
 
-    public LottoTickets() {
-    }
-
-    public LottoTickets(List<LottoTicket> lottoTickets) {
-        this.lottoTickets.addAll(lottoTickets);
-    }
-
     public List<LottoTicket> getLottoTickets() {
         return lottoTickets;
     }
@@ -32,19 +25,5 @@ public class LottoTickets {
 
     public void add(LottoTicket lottoTicket) {
         lottoTickets.add(lottoTicket);
-    }
-
-    public void addAll(LottoTickets other) {
-        lottoTickets.addAll(other.getLottoTickets());
-    }
-
-    public int getTicketTypeCount(LottoType lottoType) {
-        return (int) lottoTickets.stream()
-                .filter(lottoTicket -> lottoTicket.isType(lottoType))
-                .count();
-    }
-
-    public int getPrice() {
-        return lottoTickets.size() * LottoTicket.PRICE;
     }
 }
